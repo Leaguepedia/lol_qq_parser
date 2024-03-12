6a69f86afc253fe3e3f0dd3133fdb1a7888bed60
arbolitoloco1 <kozaksantiago@gmail.com>
2024-03-12 03:47:22 -0300

    allow reusing player data queries

--- lol_qq_parser/utils/get_player_name.py
+++ lol_qq_parser/utils/get_player_name.py
@@ -1,8 +1,13 @@
 from lol_qq_parser.utils import query_tjstats
 from lol_qq_parser.utils import Endpoints
 
+player_names_cache = {}
+
 
 def get_player_name(player_id, season_id):
+    if player_id in player_names_cache:
+        return player_names_cache[player_id]
     player_data_url = Endpoints.get_player_data_url(season_id, player_id)
     player_data = query_tjstats(player_data_url)
-    return player_data["data"][0]["playerName"] or None
+    player_names_cache[player_id] = player_data["data"][0]["playerName"] or None
+    return player_names_cache[player_id]
