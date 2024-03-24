68a5ad00f3f23f90d8f2874b95bbe38903e4da78
arbolitoloco1 <kozaksantiago@gmail.com>
2024-03-24 05:10:55 -0300

    Fix player query failing if playerId is 0

--- lol_qq_parser/utils/get_player_name.py
+++ lol_qq_parser/utils/get_player_name.py
@@ -5,6 +5,9 @@ player_names_cache = {}
 
 
 def get_player_name(player_id, season_id):
+    # Sometimes the match detail file contains playerId = 0 which makes the query fail
+    if player_id == 0:
+        return None
     if player_id in player_names_cache:
         return player_names_cache[player_id]
     player_data_url = Endpoints.get_player_data_url(season_id, player_id)
